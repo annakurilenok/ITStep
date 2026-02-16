e4b65b539ffc61b2162248e0a0c8d365b735d7d8
Sonya0904 <soph.zolotaryova01@gmail.com>
2026-02-16 21:46:52 +0300

    добавление задач в реакт

---
+++ Balans/src/js/components/App.js
@@ -0,0 +1,88 @@
+import {useState, useEffect} from 'react';
+import H1 from './H1'
+
+function uid() {
+    return `${Date.now()}_${Math.random().toString(16).slice(2)}`;
+}
+
+export default function App() {
+    const savedTasks = window.localStorage.getItem("tasks");
+
+    const [tasks, setTasks] = useState(savedTasks ? JSON.parse(savedTasks) : []);
+    const [text, setText] = useState("");
+
+    function addNewTask(event) {
+        event.preventDefault();
+
+        const newTasks = [
+            ...tasks,
+            {id: uid(), text: text, status: false,}
+        ];
+
+        setTasks(newTasks);
+        setText("");
+      
+    }
+
+    function changeStatus(taskId) {
+        const newTasks = tasks.map((task) => {
+            if(task.id == taskId) {
+                task.status = !task.status;
+            }
+            return task;
+        });
+        
+
+        setTasks(newTasks);
+      
+    }
+
+    function deleteTask(taskId) {
+        if(confirm('Вы действительно хотите удалить эту задачу?')) {
+             const newTasks = tasks.filter((task) => {
+           return task.id != taskId
+           
+        });
+        
+        setTasks(newTasks);
+        }
+        
+    }
+
+    useEffect(() => {
+        window.localStorage.setItem("tasks", JSON.stringify(tasks));
+    }, [tasks]);
+
+  
+    
+    return (
+        <>
+            <H1>Текущий баланс</H1>
+            <form className="form" onSubmit={addNewTask}>
+                <input type="date" placeholder="Дата" value={text}  onChange={ (e) => setText(e.target.value) } />
+                <input type="text" placeholder="Описание" value={text}  onChange={ (e) => setText(e.target.value) } />
+                <input type="number" placeholder="Сумма" value={text}  onChange={ (e) => setText(e.target.value) } />
+                <button type="submit">Добавить</button>
+            </form>
+            {
+                tasks.length > 0 ? <ul class="list">
+                {
+                    tasks.map(function(task){
+                        return(
+                        <li className="item" key={task.id}>
+                            <input type="checkbox" checked={task.status ? true : false} value="1" onChange={() => {changeStatus(task.id)}} />
+                            <span className={task.status ? "done" : null}>{task.text}</span>
+                            <button onClick={() => {deleteTask(task.id)}}>❌</button>
+                        </li>
+                    )
+                        }
+                    )
+                }
+            </ul> : "Список задач пустой"
+
+                
+            }
+            
+        </>
+    );
+}
